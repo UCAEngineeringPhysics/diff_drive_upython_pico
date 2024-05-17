bf17c928f32d3e476a3cc4bbb0daa5451b54b8b9
linzhanguca <lzhang12@ucae.edu>
2024-05-17 16:43:25 -0500

    Add second encoder reader. Need overclock

--- diff_drive_controller/encoded_motor_driver.py
+++ diff_drive_controller/encoded_motor_driver.py
@@ -6,38 +6,47 @@ class EncodedMotorDriver(MotorDriver):
         dir_pin_id: int, 
         pwm_pin_id: int, 
         enca_pin_id: int, 
-        # encb_pin_id: int,
+        encb_pin_id: int,
     ) -> None:
         super().__init__(dir_pin_id, pwm_pin_id)
         self.ENCA_PIN = Pin(enca_pin_id, Pin.IN, Pin.PULL_DOWN)
         self.ENCA_PIN.irq(trigger=Pin.IRQ_RISING, handler=self.inc_enca_counts)
-        # self.ENCB_PIN = Pin(encb_pin_id, Pin.IN, Pin.PULL_DOWN)
+        self.ENCB_PIN = Pin(encb_pin_id, Pin.IN, Pin.PULL_DOWN)
+        self.ENCB_PIN.irq(trigger=Pin.IRQ_RISING, handler=self.inc_encb_counts)
         # Variables
         self.enca_counts = 0
+        self.encb_counts = 0
 
     def inc_enca_counts(self, pin):
         self.enca_counts += 1
 
-    
+    def inc_encb_counts(self, pin):
+        self.encb_counts += 1
+
+
 if __name__ == "__main__":
+    machine.freq(250_000_000)
     from time import sleep
-    # m = EncodedMotorDriver(16, 17, 18)  # left
-    m = EncodedMotorDriver(14, 15, 12)  # right
+    # m = EncodedMotorDriver(16, 17, 18, 19)  # left
+    m = EncodedMotorDriver(14, 15, 12, 13)  # right
     for dc in range(0, 65536, 64):
         m.forward(dc)
-        print(f"f- dutycycle: {dc}, enca counts: {m.enca_counts}")
+        print(f"f- dutycycle: {dc}, enca counts: {m.enca_counts}, encb counts: {m.encb_counts}")
         sleep(0.01)
     for dc in reversed(range(0, 65536, 64)):
         m.forward(dc)
-        print(f"f- dutycycle: {dc}, enca counts: {m.enca_counts}")
+        print(f"f- dutycycle: {dc}, enca counts: {m.enca_counts}, encb counts: {m.encb_counts}")
         sleep(0.01)
     for dc in range(0, 65536, 64):
         m.backward(dc)
-        print(f"b- dutycycle: {dc}, enca counts: {m.enca_counts}")
+        print(f"b- dutycycle: {dc}, enca counts: {m.enca_counts}, encb counts: {m.encb_counts}")
         sleep(0.01)
     for dc in reversed(range(0, 65536, 64)):
         m.backward(dc)
-        print(f"b- dutycycle: {dc}, enca counts: {m.enca_counts}")
+        print(f"b- dutycycle: {dc}, enca counts: {m.enca_counts}, encb counts: {m.encb_counts}")
         sleep(0.01)
     print("STOP!")
     m.stop()
+    print(f"Working CPU freq: {machine.freq()}")
+    machine.freq(125_000_000)
+    print(f"Rest CPU freq: {machine.freq()}")
